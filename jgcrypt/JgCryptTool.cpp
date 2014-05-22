8ab2b3c22214abeedc33615ab149cbcde59d8f1a
zfnlj <191085859@qq.com>
2014-05-22 09:44:27 +0800

    abc

---
+++ jgcrypt/JgCryptTool.cpp
@@ -0,0 +1,276 @@
+﻿#include "JgCryptTool.h"
+#include <System/Log/log.h>
+#include <System/File/KFile.h>
+
+using namespace System::File;
+
+BOOL KEncryptRefiner::TakeFile(const char* file)
+{
+	FilePath filepath(file);
+	FilePath copypath(file); copypath.append(".copy");
+
+	KInputFileStream fi;
+	if(!fi.Open(filepath.c_str()))
+	{
+		Log(LOG_ERROR, "encrypt fail open, %s", filepath.c_str());
+		return FALSE;
+	}
+
+	JgEncStream esi;
+	if(!esi.setInputStream(&fi))
+	{
+		Log(LOG_ERROR, "encrypt fail input, %s", filepath.c_str());
+		return FALSE;
+	}
+
+	if(esi.m_encMode)
+	{
+		Log(LOG_ERROR, "not changed %s", filepath.c_str());
+		return TRUE;
+	}
+
+	KOutputFileStream fo;
+	if(!fo.Open(copypath.c_str(), "w+b"))
+	{
+		Log(LOG_ERROR, "encrypt open new, %s", copypath.c_str());
+		return FALSE;
+	}
+
+	JgEncStream eso;
+	eso.setOutputStream(&fo, 4);
+
+	char buf[1024];
+	while(TRUE)
+	{
+		int len = esi.ReadData(buf, sizeof(buf));
+		if(len < 1) break;
+		eso.WriteData(buf, len);
+	}
+
+	fi.Close();
+	fo.Close();
+
+	if(!DeleteFile(filepath.c_str()))
+	{
+		Log(LOG_ERROR, "error: encrypt delete %s", filepath.c_str());
+		return FALSE;
+	}
+
+	if(!MoveFile(copypath.c_str(), filepath.c_str()))
+	{
+		Log(LOG_ERROR, "error: encrypt move %s -> %s", copypath.c_str(), filepath.c_str());
+		return FALSE;
+	}
+
+	Log(LOG_ERROR, "encrypt %s", filepath.c_str());
+	return TRUE;
+}
+
+BOOL KDecryptRefiner::TakeFile(const char* file)
+{
+	FilePath filepath(file);
+	FilePath copypath(file); copypath.append(".copy");
+
+	KInputFileStream fi;
+	if(!fi.Open(filepath.c_str()))
+	{
+		Log(LOG_ERROR, "decrypt fail open, %s", filepath.c_str());
+		return FALSE;
+	}
+
+	JgEncStream esi;
+	if(!esi.setInputStream(&fi))
+	{
+		Log(LOG_ERROR, "decrypt fail input, %s", filepath.c_str());
+		return FALSE;
+	}
+
+	if(!esi.m_encMode)
+	{
+		Log(LOG_ERROR, "not changed %s", filepath.c_str());
+		return TRUE;
+	}
+
+	KOutputFileStream fo;
+	if(!fo.Open(copypath.c_str(), "w+b"))
+	{
+		Log(LOG_ERROR, "decrypt open new, %s", copypath.c_str());
+		return FALSE;
+	}
+
+	JgEncStream eso;
+	eso.setOutputStream(&fo, 0);
+
+	char buf[1024];
+	while(TRUE)
+	{
+		int len = esi.ReadData(buf, sizeof(buf));
+		if(len < 1) break;
+		eso.WriteData(buf, len);
+	}
+
+	fi.Close();
+	fo.Close();
+
+	if(!DeleteFile(filepath.c_str()))
+	{
+		Log(LOG_ERROR, "error: decrypt delete %s", filepath.c_str());
+		return FALSE;
+	}
+
+	if(!MoveFile(copypath.c_str(), filepath.c_str()))
+	{
+		Log(LOG_ERROR, "error: decrypt move %s -> %s", copypath.c_str(), filepath.c_str());
+		return FALSE;
+	}
+
+	Log(LOG_ERROR, "decrypt %s", filepath.c_str());
+	return TRUE;
+}
+
+JgCryptTool::JgCryptTool()
+{
+
+}
+
+JgCryptTool::~JgCryptTool()
+{
+
+}
+
+BOOL JgCryptTool::EncryptFile(const char* file)
+{
+	KEncryptRefiner refiner;
+	Log(LOG_ERROR, ">>>> start encrypt %s", file);
+	BOOL bRet = this->refineFile(file, &refiner);
+	Log(LOG_ERROR, ">>>> end encrypt %s", file);
+	return bRet;
+}
+
+BOOL JgCryptTool::EncryptDir(const char* dir)
+{
+	KEncryptRefiner refiner;
+	Log(LOG_ERROR, ">>>> start encrypt %s", dir);
+	BOOL bRet = this->refineDir(dir, &refiner);
+	Log(LOG_ERROR, ">>>> end encrypt %s", dir);
+	return bRet;
+}
+
+BOOL JgCryptTool::EncryptDirRecursive(const char* dir)
+{
+	KEncryptRefiner refiner;
+	Log(LOG_ERROR, ">>>> start recursive encrypt %s", dir);
+	BOOL bRet = this->refineDirRecursive(dir, &refiner);
+	Log(LOG_ERROR, ">>>> end recursive encrypt %s", dir);
+	return bRet;
+}
+
+BOOL JgCryptTool::DecryptFile(const char* file)
+{
+	KDecryptRefiner refiner;
+	Log(LOG_ERROR, ">>>> start decrypt %s", file);
+	BOOL bRet = this->refineFile(file, &refiner);
+	Log(LOG_ERROR, ">>>> end decrypt %s", file);
+	return bRet;
+}
+
+BOOL JgCryptTool::DecryptDir(const char* dir)
+{
+	KDecryptRefiner refiner;
+	Log(LOG_ERROR, ">>>> start decrypt %s", dir);
+	BOOL bRet = this->refineDir(dir, &refiner);
+	Log(LOG_ERROR, ">>>> end decrypt %s", dir);
+	return bRet;
+}
+
+BOOL JgCryptTool::DecryptDirRecursive(const char* dir)
+{
+	KDecryptRefiner refiner;
+	Log(LOG_ERROR, ">>>> start recursive decrypt %s", dir);
+	BOOL bRet = this->refineDirRecursive(dir, &refiner);
+	Log(LOG_ERROR, ">>>> end recursive decrypt %s", dir);
+	return bRet;
+}
+
+BOOL JgCryptTool::refineFile(const char* file, IFileRefiner* refiner)
+{
+	m_pendingDirs.clear();
+	return refiner->TakeFile(file);
+}
+
+BOOL JgCryptTool::refineDir(const char* dir, IFileRefiner* refiner)
+{
+	m_pendingDirs.clear();
+	return this->_refineDir(dir, refiner);
+}
+
+BOOL JgCryptTool::refineDirRecursive(const char* dir, IFileRefiner* refiner)
+{
+	m_pendingDirs.clear();
+
+	System::Collections::KString<512> firstDir(dir);
+	m_pendingDirs.push_back(firstDir);
+
+	do 
+	{
+		System::Collections::KString<512> folder = m_pendingDirs[0];
+		m_pendingDirs.erase(0);
+
+		this->_refineDir(folder.c_str(), refiner);
+	}
+	while (m_pendingDirs.size());
+
+	return TRUE;
+}
+
+BOOL JgCryptTool::_refineFile(const char* file, IFileRefiner* refiner)
+{
+	return refiner->TakeFile(file);
+}
+
+BOOL JgCryptTool::_refineDir(const char* dir, IFileRefiner* refiner)
+{
+	KDirectory folder;
+	if(!folder.Open(dir))
+	{
+		Log(LOG_ERROR, "error: crypt tool open dir %s", dir);
+		return FALSE;
+	}
+
+	KDirectory::FileList flist;
+	KFileSuffixFilter emptyFilter("");
+	KFileSuffixMultiFilter filter2(".txt|.cfg|.xml|.lua");
+
+	if(!folder.ListFile(flist, &emptyFilter))
+	{
+		Log(LOG_ERROR, "error: crypt tool, list files, %s", dir);
+		return FALSE;
+	}
+
+	int c = flist.size();
+	System::Collections::KString<512> fullpath;
+
+	for(int i=0; i<c; i++)
+	{
+		KFileData& fdata = flist[i];
+
+		if(fdata.m_shortName[0] == '.')
+			continue;
+
+		fullpath = fdata.m_dirName;
+		fullpath.append('/');
+		fullpath.append(fdata.m_shortName);
+
+		if(KFileUtil::IsDir(fullpath.c_str()))
+		{
+			m_pendingDirs.push_back(fullpath);
+			continue;
+		}
+		if(filter2.Accept(fdata))
+		{
+			this->_refineFile(fullpath.c_str(), refiner);
+		}
+	}
+
+	return TRUE;
+}
---
+++ jgcrypt/JgCryptTool.h
@@ -0,0 +1,58 @@
+﻿#ifndef _JG_CRYPT_TOOL_H_
+#define _JG_CRYPT_TOOL_H_
+
+#include <System/Collections/KString.h>
+#include <System/Collections/KVector.h>
+#include <System/Cipher/jgencryptor.h>
+
+typedef System::Collections::KString<512> FilePath;
+typedef System::Collections::KVector<FilePath> FilePathArray;
+
+class IFileRefiner
+{
+public:
+	virtual ~IFileRefiner() { }
+	virtual BOOL TakeFile(const char* file) = 0;
+};
+
+class KEncryptRefiner : public IFileRefiner
+{
+public:
+	BOOL TakeFile(const char* file);
+};
+
+class KDecryptRefiner : public IFileRefiner
+{
+public:
+	BOOL TakeFile(const char* file);
+};
+
+class JgCryptTool
+{
+public:
+	JgCryptTool();
+	~JgCryptTool();
+
+public:
+	BOOL EncryptFile(const char* file);
+	BOOL EncryptDir(const char* dir);
+	BOOL EncryptDirRecursive(const char* dir);
+
+	BOOL DecryptFile(const char* file);
+	BOOL DecryptDir(const char* dir);
+	BOOL DecryptDirRecursive(const char* dir);
+
+public:
+	BOOL refineFile(const char* file, IFileRefiner* refiner);
+	BOOL refineDir(const char* dir, IFileRefiner* refiner);
+	BOOL refineDirRecursive(const char* dir, IFileRefiner* refiner);
+
+private:
+	BOOL _refineFile(const char* file, IFileRefiner* refiner);
+	BOOL _refineDir(const char* dir, IFileRefiner* refiner);
+
+private:
+	FilePathArray m_pendingDirs;
+};
+
+#endif
