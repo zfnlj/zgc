1d03876696516c2a133fdb30792c14c7a4d3bd9c
fanan_zhu <zhufanan@gmail.com>
2014-05-24 20:41:06 +0800

    tbd

--- Resources/data/Quest/QuestScripts/Q_10009.lua
+++ Resources/data/Quest/QuestScripts/Q_10009.lua
@@ -1,17 +1,17 @@
 ﻿-- Script for Quest 10001 摸底考试
 -- [doneTalk=0;cate=0;failTalk=0;note=0;viewTalk=0;hard2=0]
-Quest_10004 = 
+Quest_10009 = 
 {
 Initialize = function(quest)
-	quest:SetAttrs("prevQuest=10003;abandon=0;acceptLevel=1;level=1;exp=10;money=5;");
+	quest:SetAttrs("prevQuest=10008;abandon=0;acceptLevel=1;level=1;exp=10;money=5;");
 	quest:SetAttrs("nameId=100010;talk=0;desc=100011;finishDesc=1500511;noFinishDesc=1500512;");
 	quest:SetAttrs("type=1;sheetType=1;reuse=0;hardDegree=1;achieveId=0;share=0;");
-	quest:SetAttrs("nextQuest=10005;achievement=0;timeperiod=0;");
-	quest:SetAttrs("battleField=4;");
+	quest:SetAttrs("nextQuest=10010;achievement=0;timeperiod=0;");
+	quest:SetAttrs("battleField=9;");
 	
 	-- Group 0
 	local group0 = quest:CreateTargetGroup("and");
-        group0:CreateTarget("KillMonster", "npc=10004;count=1;");
+        group0:CreateTarget("KillMonster", "npc=10009;count=1;");
   -- end groups
 	return true;
 end,
