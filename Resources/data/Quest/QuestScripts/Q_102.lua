d32d01797740fe3f2d61f9209b7c8035483c62b0
fanan_zhu <zhufanan@gmail.com>
2014-06-23 23:02:56 +0800

    abc

--- Resources/data/Quest/QuestScripts/Q_102.lua
+++ Resources/data/Quest/QuestScripts/Q_102.lua
@@ -7,17 +7,17 @@ Initialize = function(quest)
 	quest:SetAttrs("nameId=102;talk=0;desc=102;finishDesc=1500511;");
 	quest:SetAttrs("type=3;sheetType=1;reuse=0;hardDegree=1;achieveId=0;");
 	quest:SetAttrs("nextQuest=0;achievement=0;timeperiod=0;");
-	quest:SetAttrs("battleField=1;");
+	quest:SetAttrs("battleField=102;");
 	
 	-- Group 0
 	local group0 = quest:CreateTargetGroup("and");
-        group0:CreateTarget("KillMonster", "npc=10001;count=1;");
+        group0:CreateTarget("KillMonster", "npc=102;count=1;");
   -- end groups
 	return true;
 end,
 
 QName = function (quest , player)
-	return "daily mission";
+	return "daily mission2";
 end,  
 
 
--- Resources/data/Quest/QuestScripts/Q_103.lua
+++ Resources/data/Quest/QuestScripts/Q_103.lua
@@ -1,23 +1,23 @@
-﻿-- Script for Quest 101 每日任务
+﻿-- Script for Quest 102 每日任务
 -- [doneTalk=0;cate=0;failTalk=0;note=0;viewTalk=0;hard2=0]
-Quest_101 = 
+Quest_103 = 
 {
 Initialize = function(quest)
-	quest:SetAttrs("prevQuest=0;abandon=0;acceptLevel=1;level=1;exp=50;money=30;");
-	quest:SetAttrs("nameId=101;talk=0;desc=101;finishDesc=1500511;");
+	quest:SetAttrs("prevQuest=0;abandon=0;acceptLevel=1;level=1;exp=60;money=30;");
+	quest:SetAttrs("nameId=102;talk=0;desc=102;finishDesc=1500511;");
 	quest:SetAttrs("type=3;sheetType=1;reuse=0;hardDegree=1;achieveId=0;");
 	quest:SetAttrs("nextQuest=0;achievement=0;timeperiod=0;");
-	quest:SetAttrs("battleField=101;");
+	quest:SetAttrs("battleField=103;");
 	
 	-- Group 0
 	local group0 = quest:CreateTargetGroup("and");
-        group0:CreateTarget("KillMonster", "npc=101;count=1;");
+        group0:CreateTarget("KillMonster", "npc=103;count=1;");
   -- end groups
 	return true;
 end,
 
 QName = function (quest , player)
-	return "daily mission1";
+	return "daily mission3";
 end,  
 
 
@@ -25,7 +25,7 @@ GiftDesc = function (quest , player)
   local exp
 	local money
 	local itemlist
-		exp = 50			-- 经验奖励
+		exp = 60			-- 经验奖励
 		money = 30	  	-- 金钱奖励
 	return GetQuestGiftStr(itemlist,exp,money)
 end, 
